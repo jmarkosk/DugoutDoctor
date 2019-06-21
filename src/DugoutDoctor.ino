3b25f92ec8365b4d54349756f81a9ce77fb7df1f
jmarkosk <jmarkosk@gmail.com>
2019-06-21 15:58:59 -0600

    Setup

--- src/DugoutDoctor.ino
+++ src/DugoutDoctor.ino
@@ -5,6 +5,12 @@
  * Date:
  */
 
+//#define THINGSPEAK_CHANNEL_ID 411366
+//#define THINGSPEAK_API_KEY_WRITE 57VJCNFL5L8CRQ64
+
+
+
+SYSTEM_THREAD(ENABLED);
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
