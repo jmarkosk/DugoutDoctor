4928be5618d385a5f9197e738a405227b095a49c
jmarkosk <jmarkosk@gmail.com>
2019-06-23 11:14:47 -0600

    test

---
+++ src/DugoutDoctor.cpp
@@ -0,0 +1,33 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "e:/03_Engineering/01_Software/05_DugoutDoctor/src/DugoutDoctor.ino"
+/*
+ * Project DugoutDoctor
+ * Description:
+ * Author: 
+ * Date:
+ */
+
+//#define THINGSPEAK_CHANNEL_ID 411366
+//#define THINGSPEAK_API_KEY_WRITE 57VJCNFL5L8CRQ64
+
+
+
+void setup();
+void loop();
+#line 13 "e:/03_Engineering/01_Software/05_DugoutDoctor/src/DugoutDoctor.ino"
+SYSTEM_THREAD(ENABLED);
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
